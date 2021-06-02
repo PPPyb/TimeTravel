e306af52ce1742c0dbbd883011796616134fb0bf
15094906335 <18722050@bjtu.edu.cn>
2021-06-02 19:57:17 +0800

    version point7

---
+++ core/assets/maps/FireRoomMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Map007" tilewidth="30" tileheight="30" tilecount="180" columns="15">
+ <image source="C:/Users/12554/Desktop/Map007.png" width="456" height="360"/>
+</tileset>
