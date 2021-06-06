c98adb74edaaa9c01fbc4b3525933cb371d0c4f1
“PYB” <cxyxpyb@163.com>
2021-06-06 22:25:03 +0800

    UI

--- core/src/com/mygdx/timetravel/GUIRenderer.java
+++ core/src/com/mygdx/timetravel/GUIRenderer.java
@@ -16,6 +16,7 @@ public class GUIRenderer {
     Texture victory;
     Texture failed;
     Texture warmheart;
+    Texture box;
 
     BitmapFont myfont;
     public GUIRenderer(Level level)
@@ -29,6 +30,7 @@ public class GUIRenderer {
         failed = new Texture(Gdx.files.internal("GUI/failed.png"));
         warmheart = new Texture(Gdx.files.internal("GUI/warmheart.png"));
         victory = new Texture(Gdx.files.internal("GUI/victory.png"));
+        box = new Texture(Gdx.files.internal("GUI/box.png"));
     }
     public void render(Batch batch)
     {
@@ -47,21 +49,23 @@ public class GUIRenderer {
         //
         if(!level.curPlayer.isAlive)
             batch.setColor(1,0,0,1);
-        batch.draw(level.curPlayer.imgFace,0,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*4,4*Constants.HPRECHEIGHT,4*Constants.HPRECHEIGHT);
+        batch.draw(level.curPlayer.imgFace,600,0,80,80);
+        batch.setColor(1,1,1,1);
+        batch.draw(box,560,0,160,80);
         //
         batch.setColor(0.5f,0.5f,0.5f,0.75f);
-        batch.draw(imgHP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT,Constants.HPRECWIDTHRATE*level.curPlayer.maxHP,Constants.HPRECHEIGHT);
-        batch.draw(imgMP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*2,Constants.HPRECWIDTHRATE*level.curPlayer.maxMP,Constants.HPRECHEIGHT);
-        batch.draw(imgJP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*3,Constants.HPRECWIDTHRATE*level.curPlayer.maxJumpPoint,Constants.HPRECHEIGHT);
-        batch.draw(imgDP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*4,Constants.HPRECWIDTHRATE*level.curPlayer.maxDashPoint,Constants.HPRECHEIGHT);
+        batch.draw(imgHP,160,40,400,40);
+        batch.draw(imgJP,160,0,400,40);
+        batch.draw(imgMP,720,40,400,40);
+        batch.draw(imgDP,720,0,400,40);
         //
         batch.setColor(1,1,1,1);
-        batch.draw(imgHP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT,Constants.HPRECWIDTHRATE*level.curPlayer.curHP,Constants.HPRECHEIGHT);
-        batch.draw(imgMP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*2,Constants.HPRECWIDTHRATE*level.curPlayer.curMP,Constants.HPRECHEIGHT);
-        batch.draw(imgJP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*3,Constants.HPRECWIDTHRATE*level.curPlayer.curJumpPoint,Constants.HPRECHEIGHT);
-        batch.draw(imgDP,4*Constants.HPRECHEIGHT,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*4,Constants.HPRECWIDTHRATE*level.curPlayer.curDashPoint,Constants.HPRECHEIGHT);
+        batch.draw(imgHP,560-400*level.curPlayer.curHP/level.curPlayer.maxHP,40,400*level.curPlayer.curHP/level.curPlayer.maxHP,40);
+        batch.draw(imgJP,560-400*level.curPlayer.curJumpPoint/level.curPlayer.maxJumpPoint,0,400*level.curPlayer.curJumpPoint/level.curPlayer.maxJumpPoint,40);
+        batch.draw(imgMP,720,40,400*level.curPlayer.curMP/level.curPlayer.maxMP,40);
+        batch.draw(imgDP,720,0,400*level.curPlayer.curDashPoint/level.curPlayer.maxDashPoint,40);
         //
-        myfont.draw(batch,"HP"+(int)level.curPlayer.curHP+"/"+level.curPlayer.maxHP+ " Press T to check Tutorials",0,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*4);
+       // myfont.draw(batch,"HP"+(int)level.curPlayer.curHP+"/"+level.curPlayer.maxHP+ " Press T to check Tutorials",0,Constants.WINDOWS_HEIGHT-Constants.HPRECHEIGHT*4);
     }
     public void failedRender(float failedEffect)
     {
