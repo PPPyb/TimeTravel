04008e56b89864663063c44f0897c494184c552d
polarbearmage <2273704472@qq.com>
2021-06-09 21:27:01 +0800

    version alpha 8.00

--- core/src/com/mygdx/SmallMap/LevelFrame/Tutorials.java
+++ core/src/com/mygdx/SmallMap/LevelFrame/Tutorials.java
@@ -9,12 +9,15 @@ public class Tutorials {
 
     SpriteBatch batch;
     Texture tutorial;
+    Texture tutorial2;
     int curLevelNum = 0;
+    int tCnt = 0;
 
     public Tutorials()
     {
         batch = new SpriteBatch();
-        tutorial = new Texture("MainTitle/Tutorial.png");
+        tutorial = new Texture("MainTitle/Tutorial.jpg");
+        tutorial2 = new Texture("MainTitle/Tutorial2.jpg");
     }
     public void setCurLevelNum(int num)
     {
@@ -23,10 +26,19 @@ public class Tutorials {
     public void render()
     {
         batch.begin();
-        batch.draw(tutorial,0,0);
+        if(tCnt%2==0)
+            batch.draw(tutorial,0,0,1280,720);
+        else
+            batch.draw(tutorial2,0,0,1280,720);
         batch.end();
         if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
             CurState.curLevelNum = this.curLevelNum;
+        if(Gdx.input.isKeyJustPressed(Input.Keys.A))
+            tCnt++;
+        if(Gdx.input.isKeyJustPressed(Input.Keys.D))
+            tCnt++;
+        if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT))
+            tCnt++;
     }
 
 
